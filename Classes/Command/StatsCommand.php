cde080c545b2824239f0ba8a6b6589c75519d7b6
Sven Wappler <wappler@wappler.systems>
2025-05-12 12:58:15 +0200

    updates

---
+++ Classes/Command/StatsCommand.php
@@ -0,0 +1,117 @@
+<?php
+declare(strict_types=1);
+
+namespace WapplerSystems\Meilisearch\Command;
+
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Exception\SiteNotFoundException;
+use WapplerSystems\Meilisearch\ConnectionManager;
+use WapplerSystems\Meilisearch\Domain\Site\SiteRepository;
+use WapplerSystems\Meilisearch\System\Meilisearch\MeilisearchConnection;
+
+#[AsCommand(
+    name: 'meilisearch:stats',
+    description: 'Show meilisearch stats',
+)]
+class StatsCommand extends Command
+{
+
+
+    public function __construct(
+        readonly ConnectionManager $connectionManager,
+        readonly SiteRepository $siteRepository,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * Defines the allowed options for this command
+     *
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Show stats')
+            ->addArgument(
+                'siteIdentifier',
+                InputArgument::REQUIRED,
+                'Site identifier'
+            );
+    }
+
+    /**
+     * Geocode all records
+     *
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $siteIdentifier = (string)$input->getArgument('siteIdentifier');
+
+        try {
+            $site = $this->siteRepository->getSiteByIdentifier($siteIdentifier);
+        } catch (SiteNotFoundException $e) {
+            $output->writeln('Site ' . $siteIdentifier . ' not found');
+            return Command::FAILURE;
+        }
+
+        $connections = $this->connectionManager->getConnectionsBySite($site);
+
+        /** @var MeilisearchConnection $connection */
+        $connection = $connections[1];
+
+        $client = $connection->getService()->getClient();
+
+        $stats = $client->stats();
+        $output->writeln('Stats:');
+        $output->writeln('databaseSize: '. $stats['databaseSize']);
+        $output->writeln('usedDatabaseSize: '. $stats['usedDatabaseSize']);
+        $output->writeln('lastUpdate: '. $stats['lastUpdate']);
+        $output->writeln('Indexes:');
+
+        foreach ($stats['indexes'] as $indexName => $index) {
+
+            $rows = [];
+            $fieldDistribution = [];
+            $i = 0;
+            foreach ($index['fieldDistribution'] as $field => $value) {
+                $fieldDistribution[] = $field;
+                $i++;
+                if ($i > 5) {
+                    $fieldDistribution[] = '...';
+                    break;
+                }
+            }
+            $rows[] = [
+                $index['numberOfDocuments'],
+                $index['rawDocumentDbSize'],
+                $index['avgDocumentSize'],
+                $index['isIndexing'],
+                $index['numberOfEmbeddings'],
+                $index['numberOfEmbeddedDocuments'],
+                implode(', ', $fieldDistribution),
+            ];
+
+            $table = new Table($output);
+            $table
+                ->setHeaderTitle($indexName)
+                ->setHeaders(['numberOfDocuments', 'rawDocumentDbSize', 'avgDocumentSize', 'isIndexing', 'numberOfEmbeddings', 'numberOfEmbeddedDocuments', 'fieldDistribution'])
+                ->setVertical()
+                ->setRows($rows)
+            ;
+            $table->render();
+        }
+
+        return Command::SUCCESS;
+    }
+
+
+}
